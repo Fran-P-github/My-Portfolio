622d96c89b9656b9bed22d7a987e6aca404a10ca
Fran-P-github <franfx2017@gmail.com>
2021-09-30 01:24:18 -0300

    updated presentation and navbar

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('test');
+const nav_slide = () => {
+    const brg = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-bar');
+    console.log('Hello');
+    brg.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    }); 
+}
+
+nav_slide();
